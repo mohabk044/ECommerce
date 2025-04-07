d2df9fdb851d52f3dfec23d96020f6fcecd02178
Mohab Khaled <mohabk044@gmail.com>
2025-04-07 18:31:34 +0200

    Add .gitattributes and .gitignore.

---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product : BaseEntity<int>
+    {
+        //public int Id { get; set; } inherited from BaseEntity
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
